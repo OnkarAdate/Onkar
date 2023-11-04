55f860a882bc43256f6873ef161fa848db578a21
OnkarAdate <148568313+OnkarAdate@users.noreply.github.com>
2023-11-04 19:45:23 +0530

    Weather report

--- Weather_report/index.js
+++ Weather_report/index.js
@@ -1,40 +1,47 @@
 const http= require('https');
 const fs= require('fs');
-var requests= require('requests');
+var requests= require("requests");
 
-const homefile= fs.readFileSync("home.html", "utf-8");
+const home = fs.readFileSync( 'home.html', "utf-8");
 
-const replaceval =(tempval, orgval) => {
+const replaceval =(tempVal, orgVal) => {
 
-    let temperature = tempval.replace("{%tempval%}", orgval.main.temp);
-    temperature = temperature.replace("{%tempmin%}", orgval.main.temp_min);
-    temperature = temperature.replace("{%tempmax%}", orgval.main.temp_max);    
-    temperature = temperature.replace("{%location%}", orgval.name);
-    temperature = temperature.replace("{%country%}", orgval.sys.country);
+    let temperature = tempVal.replace("{%tempVal%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);    
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
     return temperature;
 };
 
 
 const server = http.createServer((req, res) => {
 
-    if(req.url == "/") {
+    if( req.url == "/") {
         requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=f4a7bc96e8d9f1eacb65ae8e783bfa56")
 
         .on("data", (chunk) => {
             const  objdata = JSON.parse(chunk);
             const arrdata = [objdata];
-
-            const realTimeData= arrdata.map((val)=> replaceval(homefile, val));
+            
+            const realTimeData= arrdata
+            .map((val)=> replaceval(home, val))
+            .join("");
+            res.write(realTimeData);
             console.log(realTimeData);
         })
 
         .on("end", (err)=> {
             if(err) return console.log("Error......", err);
-            res.end();
+            res.end()
         });
 
     }
 });
 
 
-server.listen(8000, "127.0.0.1");
+//server.listen(8000, "127.0.0.1");
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log(" Port is Active... ");
+});
