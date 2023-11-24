a80286272176d627057563468cacab6347b4c7cb
Onkar Adate <adateonkar@gmail.com>
2023-11-24 22:08:00 +0530

    Mongodb

---
+++ Mongojs/src/app.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+// connecting with Mongo
+mongoose.connect("mongodb://127.0.0.1:27017/House")
+.then(()=> console.log("Connection Done...."))
+.catch((err)=> console.log(err));
+
+
+//Defining Schema
+const familySchema= new mongoose.Schema({
+
+    Name :{ type: String,required :true},
+    Age:Number,
+    Mobile: Number,
+    City: String
+})
+
+//Creating collection / model
+const Member = new mongoose.model("Member", familySchema);
+
+
+//inserting data by using Async()
+const createMemberData = async( ) => {  
+
+    try{
+
+        const memberInfo1 = new Member(
+            {
+            Name : "Sujata Adate",
+            Age : 35,
+           Mobile: 9503448033,
+           City : "Sangola"
+        })
+
+        const memberInfo2 = new Member(
+            {
+            Name : "Kajal Adate",
+            Age : 25,
+           Mobile: 9503448033,
+           City : "Sangola"
+        })
+
+
+        
+        const memberData = await Member.insertMany([memberInfo1,memberInfo2])
+        console.log(memberData);
+    
+
+    }catch(error){ console.log(error.massage);}
+  
+}
+
+//calling function
+//createMemberData(); // This is Commented due to if we run Read op. then it may create duplicate data
+
+
+// read Data.
+const readData = async()=>
+{
+    try{
+        const result=await Member.find({Age:35});
+console.log(result);
+
+    }catch(err){ console.log(err.massage);}
+}
+readData();
--- Mongojs/src/index.js
+++ Mongojs/src/index.js
@@ -1,21 +1,43 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Family").then(() => console.log("Connection Done..")).catch((err)=> console.log(err));
+mongoose.connect("mongodb://127.0.0.1:27017/House")
+.then(() => console.log("Connection Done.."))
+.catch((err)=> console.log(err));
 
 
-//Schema Creation.
+ //Schema Creation.
+const houseShema = new mongoose.Schema(
+     {
+         name : { type: String,required :true},
+          city : String,
+          Age : Number,
+          date : { type : Date, default: Date.now},
+          Mob : Number
+     }
+     )
 
-const fshema = new mongoose.Schema(
-    {
-        name : { type: String,required :true},
-         city : String,
-         Age : Number,
-         date : { type : Date, default: Date.now},
-         Mob : Number
-    }
-    )
+ // Collation creating
 
-// Collation creating
+ const Fdata = new mongoose.model("Myhouse", houseShema);
 
-const Fdata = new mongoose.model("Myfami", fshema);
+ const createDocument = async() => {
+
+     try{
+
+         const onkardata = new Fdata({
+
+             name : "akash Adate",
+           City : "Sangola",
+             Age :25,
+             Mob : 9503448033
+         })
+        
+ const result = await onkardata.save();
+ console.log(result);
+     }catch(err){
+     console.log(err);
+     }
+ }
+
+ createDocument();
 
